d5cc270b54ab5cc562ee9a3afd9968f62883aa50
alice-frln <alice.frelin@edu.univ-fcomte.fr>
2025-02-07 14:17:39 +0100

    affichage photo dans route dynamique offre

--- src/backend.mjs
+++ src/backend.mjs
@@ -20,7 +20,7 @@ export async function getOffres() {
 export async function getOffre(id) {
     try {
         let data = await pb.collection('maison').getOne(id);
-        data.imageUrl = pb.files.getURL(data, data.image);
+        data.img = pb.files.getURL(data, data.image);
         return data;
     } catch (error) {
         console.log('Une erreur est survenue en lisant la maison', error);
