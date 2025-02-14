40f2f6c1e8eac691bfabc9f15930f645931f995d
alice-frln <alice.frelin@edu.univ-fcomte.fr>
2025-02-14 14:16:04 +0100

    page add astro

--- src/backend.mjs
+++ src/backend.mjs
@@ -33,3 +33,20 @@ export async function bySurface(s) {
     const surfaceRecord = await pb.collection('Maison').getFullList({ filter : `surface> ${s}`, }) ;
     return surfaceRecord ;
     }
+
+
+export async function addOffre(house) {
+    try {
+        await pb.collection('maison').create(house);
+        return {
+            success: true,
+            message: 'Offre ajoutée avec succès'
+        };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant la maison', error);
+        return {
+            success: false,
+            message: 'Une erreur est survenue en ajoutant la maison'
+        };
+    }
+}
